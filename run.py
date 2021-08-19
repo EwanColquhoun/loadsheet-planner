792c1eaeee43c5200698e74fac0352266e871376
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-08-19 11:24:43 +0000

    Add functions main, select_aircraft and opening_text

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from functions import typing
 
 
-def select_aircraft():
+def opening_text():
     """
-    Lets the user select aircraft from the range of aircraft.
-    This sets the total fuel, passengers and weights.
+    Displays the opening text. details what aircraft are available to select.
     """
     typing("Welcome to the Loadsheet Planner\n", 0.02)
     typing("Retrieving the database of Aircraft\n", 0.02)
+    print()
     typing('                         |                         \n', 0.01)
     typing('                         |                         \n', 0.01)
     typing('                         |                         \n', 0.01)
@@ -17,14 +17,48 @@ def select_aircraft():
     typing('                    -         -                    \n', 0.01)
     typing(' \__________________:         :__________________/ \n', 0.01)
     typing("   '''.-^-.' '.-^-.''\       /''.-^-.' '.-^-.'''    \n", 0.01)
-    typing("      '___'   '___'   ''---''    '___'   '___'       \n", 0.01)
-
+    typing("      '___'   '___'   ''---''   '___'   '___'       \n", 0.01)
+    print()
     typing("You have 3 aircraft available to you:\n", 0.02)
     typing("a) Boeing 747-400\n", 0.02)
     typing("b) Embraer 190\n", 0.02)
     typing("c) Jetstream 41\n", 0.02)
+    print()
 
+
+def select_aircraft():
+    """
+    Asks the user to input which aircraft they would like to load.
+    if the input is invalid (not a,b or c) it returns an error and asks
+    the user to try again.
+    """
     aircraft_type = input("Please select the aircraft, enter a, b or c: ")
-    return aircraft_type
 
-select_aircraft()
+    if aircraft_type.lower() == 'a':
+        aircraft_a = 'Boeing 747-400'
+        print(f"You have chosen the {aircraft_a}")
+        return aircraft_a
+    elif aircraft_type.lower() == 'b':
+        aircraft_b = 'Embraer 190'
+        print(f"You have chosen the {aircraft_b}")
+        return aircraft_b
+    elif aircraft_type.lower() == 'c':
+        aircraft_c = 'Jetstream 41'
+        print(f"You have chosen the {aircraft_c}")
+        return aircraft_c
+    else:
+        print()
+        print("------That wasn't a valid aircraft------")
+        print("Please select your aircraft from the options a, b or c.\n")
+        select_aircraft()
+   
+
+def main():
+    """
+    Runs the application on loading the browser.
+    """
+    opening_text()
+    select_aircraft()
+
+
+main()
